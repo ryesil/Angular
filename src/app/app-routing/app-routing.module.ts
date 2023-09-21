089dbad018acb8c723070c527b3ad65ec8679c1e
Ramazan Yesil <85940052+ryesil@users.noreply.github.com>
2023-09-20 23:15:24 -0500

    Update app-routing.module.ts

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -10,15 +10,15 @@ import { RecipesResolverService } from '../recipes/recipes-resolver.service';
 import { AuthComponent } from '../auth/auth/auth.component';
 
 const appRoutes: Routes = [
- {path:'', redirectTo:'/recipes', pathMatch:'full'},
+ {path:'', redirectTo:'/recipes', pathMatch:'full'},//When app starts we redirect to /recipes path
 { path: 'recipes', component: RecipesComponent, children:[
   {path:'', component:RecipeStartComponent},
-  {path:'new', component:RecipeEditComponent},
+  {path:'new', component: RecipeEditComponent},
   {path:':id', component:RecipeDetailComponent, resolve:[RecipesResolverService]},
   {path:':id/edit', component:RecipeEditComponent, resolve:[RecipesResolverService]},
 ] },
-{path:'auth', component:AuthComponent},
-{path:'shopping-list', component:ShoppingListComponent}
+{path:'auth', component: AuthComponent},
+{path:'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({
