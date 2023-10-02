0cf43da949aa870cbf9f18a13445960d7ed8cbdd
ryesil <ramazanysl@gmail.com>
2023-10-01 22:19:45 -0500

    trying to deploy on Netlify

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
 import { AuthComponent } from '../auth/auth.component';
 
 
--- src/app/app-routing/shopping-list.module.ts
+++ src/app/app-routing/shopping-list.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
-import { AuthComponent } from '../auth/auth.component';
+import { ShoppingEditComponent } from '../shopping-list/shopping-edit/shopping-edit.component';
+import { FormsModule } from '@angular/forms';
 
 const routes=[
     {path:'shopping-list', component:ShoppingListComponent}
 ]
 
 @NgModule({
-    declarations:[],
-    imports:[CommonModule,
+    declarations:[ShoppingListComponent, ShoppingEditComponent],
+    imports:[CommonModule, FormsModule,
         RouterModule.forChild(routes)],
-    exports:[RouterModule]
 })
 export class ShoppingListRoutingModule{}
